2e76e390928ff458b5367c3960081bc6afee2e09
leejinwon012 <leejinwon012@naver.com>
2024-05-01 04:49:45 +0900

    상품 수정 제외 products 모든 기능 완료

--- products/urls.py
+++ products/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import ProductCreateView, ProductListView, ProductDetailView
+from .views import ProductListCreateView, ProductUpdateDeleteView
 
 urlpatterns = [
-    # 상품 등록
-    path("", ProductCreateView.as_view(), name="product-create"),
+    # 상품 등록, 목록 조회
+    path("", ProductListCreateView.as_view(), name="product-list-create"),
 
-    # 상품 목록 조회
-    path("", ProductListView.as_view(), name="product-list"),
-
-    # 상품 상세 조회, 수정, 삭제
-    path("<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+    # 상품 수정, 삭제
+    path("<int:pk>/", ProductUpdateDeleteView.as_view(), name="product-update-delete"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,29 +1,30 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.exceptions import PermissionDenied
 
 
-# 상품 등록
-class ProductCreateView(generics.CreateAPIView):
+# 상품 등록, 상품 조회
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
-# 상품 목록 조회
-class ProductListView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    permission_classes = [permissions.AllowAny]
+    def get_paginated_response(self, data):
+        paginator = PageNumberPagination()
+        paginator.page_size = 10  # 페이지당 상품 수
+        return paginator.get_paginated_response(data)
 
 
-# 상품 상세 조회, 수정, 삭제
-class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+# 상품 수정 및 삭제
+class ProductUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -36,12 +37,12 @@ class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
         instance = self.get_object()
         if instance.author != request.user:
             raise PermissionDenied("해당 권한이 없습니다.")
         serializer = self.get_serializer(
-            instance, data=request.data, partial=partial)
+            instance, data=request.data, partial=kwargs.get('partial', False)
+        )
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(serializer.data)
